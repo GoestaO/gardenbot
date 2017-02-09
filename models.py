469e44e74776d7cdb79b5b4fdebb96b209f5ef28
Gösta <goesta.o@gmail.com>
2017-02-09 21:52:51 +0100

    continued with login

--- models.py
+++ models.py
@@ -1,7 +1,11 @@
-from app import db
-#from datetime import datetime
+from app import db, login_manager
+# from datetime import datetime
 import re, datetime
 
+
+@login_manager.user_loader
+def _user_loader(user_id):
+    return User.query.get(int(user_id))
 
 def slugify(s):
     return re.sub('[^\w]+', '-', s).lower()
@@ -13,10 +17,25 @@ class User(db.Model):
     email = db.Column(db.String(64), unique=True)
     password_hash = db.Column(db.String(255))
     name = db.Column(db.String(64))
+    slug = db.Column(db.String(64), unique=True)
+    active = db.Column(db.Boolean, default=True)
     created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
 
     def __init__(self, *args, **kwargs):
         super(User, self).__init__(*args, **kwargs)  # Call parent constructor
-
+    
+    def generate_slug(self):
+        if self.name:
+            self.slug = slugify(self.name)
+    
     def __repr__(self):
         return "<User: {0}>".format(self.email)
+    
+    def get_id(self):
+        return unicode(self.id)
+    
+    def is_authenticated(self):
+        return True
+    
+    def is_active(self):
+        return self.active
