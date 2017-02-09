75add3b6d250231521624b0a71c1a01a48c7730b
Gösta <goesta.o@gmail.com>
2017-02-09 21:52:51 +0100

    continued with login

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import connexion
 import os
-from flask import Flask, Session
+from flask import Flask, Session, g
 from configuration import Configuration
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, current_user
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 app = connexion.App(__name__)
 app.add_api('api/endpoints/gardenbot-api.yaml')
 app.app.config.from_object(Configuration)
 db = SQLAlchemy(app.app)
+migrate = Migrate(app, db)
+manager = Manager(app.app)
+manager.add_command('db', MigrateCommand)
 
+login_manager = LoginManager(app.app)
+login_manager.login_view = "login"
 
-
+@app.app.before_request
+def _before_request():
+    g.user = current_user
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -6,5 +6,7 @@ class WaterForm(wtforms.Form):
     wateringTime = wtforms.IntegerField('Time (in seconds)', id='time',
                                         validators=[DataRequired(), NumberRange(min=1, max=120)])
     #key = wtforms.PasswordField('API-Key', id='api_key', validators=[DataRequired()])
-    def save_watering(self, watering):
-        return watering
+    #===========================================================================
+    # def save_watering(self, watering):
+    #     return watering
+    #===========================================================================
