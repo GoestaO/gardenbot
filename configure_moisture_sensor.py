bc2df9b26de65d249f2a9088f73836e5ecd73eee
Gösta Ostendorf <goesta.o@gmail.com>
2017-02-22 22:04:19 +0100

    Tiny script to fine tune the moisture sensor without watering.

--- configure_moisture_sensor.py
+++ configure_moisture_sensor.py
@@ -3,6 +3,9 @@ from garden import Gardenbot
 if __name__ == "__main__":
     gb = Gardenbot()
     gb.setup_pins()
+    gb.start_sensor()
+
     soil_is_dry = Gardenbot.soil_is_dry(14)
+    gb.close_sensor()
     print("Soil is dry: {}".format(soil_is_dry))
     gb.close()
