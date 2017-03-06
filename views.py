394c747c9cd4c7697e846be99873a5f077255d70
Gösta <goesta.o@gmail.com>
2017-03-06 22:45:51 +0100

    - Manual watering from web page - started with weather API

--- views.py
+++ views.py
@@ -4,6 +4,8 @@ from forms import WaterForm, LoginForm
 from app import login_manager
 from flask import g
 from models import User
+from flask_wtf import Form
+from garden import Gardenbot
 from flask_login import current_user, login_user, logout_user, login_required
 
 
@@ -13,6 +15,7 @@ def load_user(id):
 
 
 @app.route('/')
+@login_required
 def homepage():
     form = WaterForm()
     return render_template('homepage.html', form=form)
@@ -21,24 +24,36 @@ def homepage():
 @app.route('/', methods=['POST'])
 @login_required
 def water_manually():
-    form = WaterForm(request.form)
-    if form.validate():
+    form = WaterForm()
+    if form.validate_on_submit():
+        gb = Gardenbot()
+        gb.setup_pins()
+        gb.close_water()
         time = request.form.get('wateringTime')
+        gb.water_plants(watering_time=time)
         flash("Watering started for {0} seconds".format(time))
+        gb.close()
         return redirect(url_for('homepage'))
-    # else:
-    #     form = WaterForm()
+    else:
+        print("Something went wrong.")
+        form = WaterForm()
     return render_template('homepage.html', form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        form = LoginForm(request.form)
-        if form.validate():
+        form = LoginForm()
+        if form.validate_on_submit():
             login_user(form.user, remember=form.remember_me.data)
             flash("Successfully logged in as {}".format(form.user.email), "success")
             return redirect(request.args.get("next") or url_for("homepage"))
     else:
         form = LoginForm()
     return render_template("login.html", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash("You have been successfully logged out.", "success")
+    return redirect(request.args.get("next") or url_for("homepage"))
