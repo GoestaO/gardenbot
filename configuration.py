1b3465336dea33f40a58ef84a7ef47614ec95eb8
Gösta <goesta.o@gmail.com>
2017-03-25 00:10:27 +0100

    Restructured

--- configuration.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Configuration(object):
-    APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
-    SQLALCHEMY_DATABASE_URI = "mysql://gardenbot:cj4h8ofm@localhost:3306/gardenbot"
-    SECRET_KEY = '000b3d18-7f83-4515-ab66-99199cbbd074'  # Create a unique key for your app.
