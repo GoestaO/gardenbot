99cfad1c2a3c23d068fa85c685a7971b49cd858b
Gösta <goesta.o@gmail.com>
2017-03-25 00:10:27 +0100

    Restructured

--- weather.py
+++
@@ -1,7 +0,0 @@
-from pprint import pprint
-import requests
-
-
-s = "http://api.openweathermap.org/data/2.5/forecast/city?id={id}&APPID={APIKEY}".format(APIKEY="79e49fb1bc94e6f20e03087f81e4bb5e", id=2950159)
-r = requests.get(s)
-pprint(r.json())
