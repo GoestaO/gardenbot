d8498c730e3cb6728c174b302560d1102572812c
Gösta <goesta.o@gmail.com>
2017-05-08 00:13:24 +0200

    - Dockerfile created - requirements for pip created - check soil status implemented - webservice calls implemented

--- webapp/main.py
+++ webapp/main.py
@@ -1,12 +1,18 @@
 from views import *
-import connexion
 from app import app, db
 import models
+import os
+import sys
+
+dir_path = os.path.dirname(os.path.realpath(__name__))
+sys.path.extend(dir_path)
 
 
-
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # app = connexion.App(__name__)
     # app.add_api('api/endpoints/gardenbot-api.yaml')
 
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    # app.run(debug=True, host='0.0.0.0', port=8080)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
