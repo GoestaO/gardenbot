4aab427113c01a5459e2d6e84d4b9cf9734ee5fa
Gösta <goesta.o@gmail.com>
2017-05-12 23:14:56 +0200

    - Dockerfile created - requirements for pip created - check soil status implemented - webservice calls implemented

--- webapp/configuration.py
+++ webapp/configuration.py
@@ -3,5 +3,5 @@ import os
 class Configuration(object):
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     APPLICATION_ROOT = "gardenbot"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://gardenbot:gardenbot@localhost:3306/gardenbot"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://gardenbot:gardenbot@osmc.local:3306/gardenbot"
     SECRET_KEY = '000b3d18-7f83-4515-ab66-99199cbbd074'  # Create a unique key for your app.
--- webapp/webservice_clients.py
+++ webapp/webservice_clients.py
@@ -9,6 +9,7 @@ base_url_test = 'http://gardenbot.local:5000/v1.0/'
 
 def water_plants(seconds):
     url = "{}/watering/{}".format(base_url, str(seconds))
+    print(url)
     r = requests.post(url=url, headers=headers)
     return r
 
@@ -18,6 +19,7 @@ def check():
     header = {'Content-Type': 'application/json', 'Accept': 'Accept: text/html',
                'API-Key': '000b3d18-7f83-4515-ab66-99199cbbd074'}
     url = "{}check".format(base_url)
+    print(url)
     r = requests.post(url=url, headers=header)
     return r.text
 
