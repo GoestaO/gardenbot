0f5786b7941d678f2ae0b21eefba0b070d57548e
Gösta <goesta.o@gmail.com>
2017-05-23 21:48:06 +0200

    - Added virtualenv to project - some minor improvements in the frontend

--- webapp/views.py
+++ webapp/views.py
@@ -22,7 +22,7 @@ def homepage():
 
 @app.route('/water', methods=['GET'])
 def water():
-    response = gardenbot_client.water_plants(10)
+    response = gardenbot_client.water_plants(30)
     return jsonify(response.text, response.status_code)
 
 
