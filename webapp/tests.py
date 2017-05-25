19adf7ba31c559ea87f4c2681127535bff3249d4
Gösta <goesta.o@gmail.com>
2017-05-25 13:13:16 +0200

    Started with some tests

--- webapp/tests.py
+++ webapp/tests.py
@@ -2,7 +2,10 @@ import unittest
 from main import app, db
 from models import User
 import requests
-
+from forms import LoginForm
+from flask import request, Response
+from flask_login import login_user
+import json
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -10,7 +13,8 @@ class TestCase(unittest.TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://gardenbot:gardenbot@localhost:3306/gardenbottest"
         self.app = app.test_client()
         db.create_all()
-        self.createUser()
+        TestCase.createTestUser()
+        self.loginForm = TestCase.createTestLoginForm()
 
     def tearDown(self):
         db.session.remove()
@@ -18,36 +22,46 @@ class TestCase(unittest.TestCase):
         pass
 
     @staticmethod
-    def createUser():
+    def createTestLoginForm():
+        """Creates a pre filled LoginForm to test login fucntionality"""
+
+        login_form = LoginForm()
+        login_form.email.data = "test123@huhu.com"
+        login_form.password.data = "huhu"
+        return login_form
+
+    @staticmethod
+    def createTestUser():
         u = User.create("test123@huhu.com", "huhu")
         db.session.add(u)
         db.session.commit()
 
-    def login(self, username, password):
-        return self.app.post('/login', data=dict(
-            username=username,
-            password=password
-        ), follow_redirects=True)
-
     def logout(self):
         return self.app.get('/logout', follow_redirects=True)
 
-        ''' sends HTTP GET request to the application
-        on the specified path '''
     def test_home(self):
+        """sends HTTP GET request to the application
+               on the specified path"""
         response = self.app.get("/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200, "Error: '/' is 404")
 
     def test_status(self):
         response = self.app.get("/status")
-        self.assertEqual(response.status_code, 200)
-
+        self.assertEqual(response.status_code, 200, "Error: /status is 404")
 
     def test_authenticate(self):
         test_user = User.authenticate("test123@huhu.com", "huhu")
-        self.assertIsInstance(test_user, User)
+        self.assertIsNotNone(test_user, "Authenticate error: Is not None")
+        self.assertIsInstance(test_user, User, "test_user is not an instance of User")
 
+    def test_loginForm_validation(self):
+        self.assertTrue(self.loginForm.validate(), "Error in validation of LoginForm")
 
+    def test_login_post(self):
+        data = dict(email="gösta@huhu.com", password="huhu")
+        content_type = "application/x-www-form-urlencoded"
+        response = self.app.post('/login', data=data, content_type=content_type)
+        self.assertTrue(response.status_code is not None, "Error in Login via Post: Not found")
 
 
 if __name__ == '__main__':
