c2c1ae71845e1fe0598303846a1f96b15dd7b1de
Gösta Ostendorf <gostendorf@zalando.de>
2018-05-14 14:21:04 +0200

    Fix db query

--- api/database/db.py
+++ api/database/db.py
@@ -22,10 +22,10 @@ def persist(entity):
 
 def get_water_history_from_db():
     session = Session()
-    sql = "select * from (SELECT UNIX_TIMESTAMP(timestamp(date(protocol.timestamp), '02:00:00.000'))*1000 as millis," \
-          "count(protocol.water) as waterings FROM protocol" \
-          "GROUP BY date(protocol.timestamp)" \
-          "ORDER BY date(protocol.timestamp)" \
+    sql = "select * from (SELECT UNIX_TIMESTAMP(timestamp(date(protocol.timestamp), '02:00:00'))*1000 as millis, " \
+          "count(protocol.water) as waterings FROM protocol " \
+          "GROUP BY date(protocol.timestamp) " \
+          "ORDER BY date(protocol.timestamp) " \
           "desc limit 5)sub order by sub.millis asc;"
     result = session.execute(sql).fetchall()
     session.close()
